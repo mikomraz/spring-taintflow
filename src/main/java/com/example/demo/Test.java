efe32e6d4678ab014d3a01a458f4286612af917b
Michael Komraz <mikomraz@gmail.com>
2020-05-07 20:06:31 +0300

    make the trivial even more trivial

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -4,11 +4,7 @@ import java.io.IOException;
 
 class Test {
     public static void main(String[] args) throws IOException {
-
         String script = System.getenv("SCRIPTNAME");
-        if (script != null) {
-            // BAD: The script to be executed is controlled by the user.
-            Runtime.getRuntime().exec(script);
-        }
+        Runtime.getRuntime().exec(script);
     }
 }
